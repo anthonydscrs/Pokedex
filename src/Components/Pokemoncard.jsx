29a3a11a537a31febcabd81dc09a303784c09289
anthonydscrs <[andiscours@laposte.net](mailto:andiscours@laposte.net)>
2023-03-28 11:48:35 +0200

    new

--- src/Components/Pokemoncard.jsx
+++ src/Components/Pokemoncard.jsx
@@ -1,12 +1,22 @@
-
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  },
+];
 
 function PokemonCard() {
+  const pokemon = pokemonList[0];
+  const pokemonImg = pokemon.imgSrc;
 
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur"></img>
-    <figcaption>bulbasaur</figcaption>
-    </figure>;
-  
-  }
-  
-  
-  export default PokemonCard;
+  return (<figure>
+    {pokemonImg === undefined ? <p>???</p> : <img src={pokemonImg} alt={pokemon.name}></img> }
+    <figcaption>{pokemon.name}</figcaption>
+  </figure>);
+}
+
+export default PokemonCard
