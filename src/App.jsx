ffe73a317c7f8e90b59cccb03106e6d7feef2663
anthonydscrs <[andiscours@laposte.net](mailto:andiscours@laposte.net)>
2023-03-30 10:30:51 +0200

    button

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,54 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from "./components/PokemonCard.jsx"
 
-function App(props) {
-  const [count, setCount] = useState(0)
 
-  const pokemonList = [
-    {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+const pokemonList = [
+
+  {
+  
+      name: "bulbasaur",
+  
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  
     },
-    {
-      name: "Mew",
+  
+    { name: "charmander",
+      imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  
+    },
+  
+    { name: "squirtle",
+      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  
+    },
+  
+    { name: "pikachu",
+     imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+
+    },
+
+    {name: "mew",
     },
   ];
 
-  let pokemon = pokemonList[0];
+function App(props) {
+  let [count, setCount] = useState(0)
+  let pokemon = pokemonList[count]
+
+    const handleClickPrevious = () => {
+      setCount(count - 1)
+    }
+    const handleClickNext = () => {
+      setCount(count + 1)
+    }
 
   return (
-    <div className="App">
-      <PokemonCard props={pokemon} />
-    </div>
-  )
-}
+  <div className="App">
+  <PokemonCard props={pokemon} />
+<button onClick={handleClickPrevious}>Previous</button>
+<button onClick={handleClickNext}>Next</button>
+ </div>
+ )
+  }
 
 export default App;
